ca21e74bbd5129bee129873e799d87fa86bcf5f0
Gizem <gizemny@gmail.com>
2016-03-03 18:23:14 -0600

    added model relationships

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+	public function lane() {
+		$this->belongsTo('\App\Lane');
+	}
+
+	public function tags() {
+		$this->belongsToMany('\App\Tag');
+	}
 }
