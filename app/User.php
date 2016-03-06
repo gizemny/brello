8f78a0872ce63e0511d2616b436e2fb873bab018
Gizem <gizemny@gmail.com>
2016-03-05 19:28:28 -0600

    added relationships to user model

--- app/User.php
+++ app/User.php
@@ -22,5 +22,14 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password', 'remember_token',
-    ];
+    ];  
+    public function lanes() {
+        return $this->hasMany('\App\Lane');
+    } 
+    public function tasks() {
+        return $this->hasMany('\App\Task');
+    } 
+    public function tags() {
+        return $this->hasMany('\App\Tag');
+    }          
 }
